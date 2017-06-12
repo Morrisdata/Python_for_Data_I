75a8144e8389eecdce718ffddc6de1daa83428e2
Matthew Morris <matthewmorris.da@gmail.com>
2017-06-12 07:37:37 -0700

    Create ex.py

---
+++ Solutions/ex.py
@@ -0,0 +1,10 @@
+#1. Print the name of the mom.
+
+family['mom']
+
+#2. Change the size to 5
+family['size']= 5
+
+#3. Add 'Hank2' to the list of kids. Note-In this tv series the kids are clone to 5
+family['kids']=['hank', 'dean', 'hank2']
+family
