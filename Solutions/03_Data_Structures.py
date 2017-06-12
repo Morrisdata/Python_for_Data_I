223b66a1fba3d830a588a015fba50129cfdfb801
Matthew Morris <matthewmorris.da@gmail.com>
2017-06-12 08:04:36 -0700

    Create 03_Data_Structures.py

--- Solutions/03_Data_Structures.py
+++ Solutions/03_Data_Structures.py
@@ -8,3 +8,11 @@ family['size']= 5
 #3. Add 'Hank2' to the list of kids. Note-In this tv series the kids are clone to 5
 family['kids']=['hank', 'dean', 'hank2']
 family
+#4 #4. Fix 'hank' and 'dean' so that the first letter is capitalized.
+family['kids'][0]='Hank'
+family['kids'][1]='Dean'
+family['kids'][2]='Hank2'
+family
+
+#5 Add to the family dictionary bodygaurd 'Brock'
+family['bodygaurd'] = 'Brock'
