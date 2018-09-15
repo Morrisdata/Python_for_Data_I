5a999545890eef9428c6655404006f24415a6f6e
Matthew Morris <matthewmorris.da@gmail.com>
2018-09-15 08:27:32 -0700

    Update 03_python_for_data_I_conditionals&functions.py

--- New_Code/03_python_for_data_I_conditionals&functions.py
+++ New_Code/03_python_for_data_I_conditionals&functions.py
@@ -1,3 +1,8 @@
+# Python for Data I
+# Matthew Morris 
+# Matthewmorris.da@gmail.com
+# dev2018 
+
 ###############################################################################
 ##                INPUT, Arguements and IF THEN ELIF logic                   ##
 ###############################################################################		
@@ -5,18 +10,20 @@
 # When you run this you will have a prompt but no directions
 	
 answer = input() 
+
+# It's always a good idea to give directions with your input prompts
 answer2 = input('Ask a question...  :')
 answer2
 
 
 ## EXERCISE
 ''' 
-review the function below
+Review and fix the function below
 q1 has not question for a prompt so this will need to be fixed
 q2 doesn't even have a input or question
 q3 is missing entirely
 
-see if you can fix the code	
+see if you can fix the code You may need to refer to prior unit. 
 '''
 
 	
@@ -32,7 +39,7 @@ def ritest():
     print "the way it works is: {} 
     print "and an example of why you would use this would be "		
 
-
+ritest()
 
 
 
@@ -42,13 +49,17 @@ def ritest():
 # print_none, print_one, print_two
 
 def print_none():
-    print "Even though this has no arguements it is still important"
+    print ("Even though this has no arguements it is still important")
 print_none()
 
 #This function will take 1 arguement
 def print_one(x):
     print (x) # 
 print_one("x allows me to pass in a usable variable")
+
+
+
+
 print_one()
 
 # This function will take 2 arguements
@@ -60,19 +71,21 @@ print_two(2+3, "< that number is the x variables/arguement. This is the y")
 
 
 # Math functions and arguements
-a=10
-b=5
+
+# addition
 def add(a, b):
    return a + b
-add(a,b)
+add(10, 20)
 
+
+# subtraction
 a=10
 b=5
 def subtract(a, b):
    return a - b
 subtract(a,b)
 
-
+#division
 def divide(a, b):
     a=10
     b=5
@@ -80,17 +93,12 @@ def divide(a, b):
 divide(a,b)
 
 # EXERCISE:
-# Write a function that takes two parameters (hours and rate), and returns the total pay.
-# Bonus - Use raw_input to make the variables dynamic
-# Total pay
+'''
+Write a function that takes two parameters (hours and rate), and returns the total pay.
+Bonus - Use raw_input to make the variables dynamic
+Total pay
+'''
 
-hours = 10
-rate = 1000
-
-
-def moula(hours,rate):
-    return hours*rate
-moula(hours,rate)
 
 
 
@@ -98,7 +106,7 @@ moula(hours,rate)
 '''
 Use input prompts, passing objects into a string and arguements to create
 a Calculator that outputs a message after use. If you cannot think of a calculator
-you can modify one from above
+you can modify one from above. Consider calculations you use on a daily basis.
 '''
 
 
@@ -108,25 +116,27 @@ you can modify one from above
 
 
 # Conditionals pineapple pizza debate
+## 3 value logic returns TRUE, FALSE or UNKNOWN. This is powerful logic
+
 people = 100.00
-pineapple = 40.00
-specials = 
-antip =60.00
+pineapple = 49.00
+antip =51.00
 paying = "Terry"
 
     
-if (pineapple/people)*100 > 50: print "pinapple does belong on pizza"
-else: print "pineapple does not belong on pizza"
+if (pineapple/people)*100 > 50: print ("pinapple does belong on pizza")
+else: print ("pineapple does not belong on pizza")
 
 
-if (pineapple/people) *100 > 50 or paying == "Terry": print "pinapple does belong on pizza"
-else: print "pineapple does not belong on pizza"
+if (pineapple/people) *100 > 50 or paying == "Terry": print ("pinapple does belong on pizza")
+else: print ("pineapple does not belong on pizza")
 
 
+# Multiple filters and conditionals that are covered
 weather = "Sunny"
 day = "Monday"
 time = "5am"
-season = "Winter"
+season = "SUMMER"
 
 
 if weather == "Sunny" and day == "Monday" and time == "5am" and season == "Winter": 
@@ -157,7 +167,6 @@ If it is under 2% bring back directions to markup
 If it is in range bring back directions to keep same
 '''
 ##
-
 #IMU evaluation and suggestions.
 
 
@@ -166,11 +175,10 @@ If it is in range bring back directions to keep same
 
 ''' PROJECT EXERCISE
 Revisit your previous project and consider how you might use 
-If then else, Math, arguements, raw_input and conditionals to update your 
+If then else, Math, arguements, input and conditionals to update your 
 scores, find your averages and give recommendations and what you can add to
 your resume or what you need to practice.
 Could you use input with If then else to create a Quiz? Maybe later you can
 begin grading the quiz.
 '''
-
 
