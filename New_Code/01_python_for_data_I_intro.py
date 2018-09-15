8f7f4b4b9d7e7a528d7f4f69f2a64494a377f210
Matthew Morris <matthewmorris.da@gmail.com>
2018-09-15 08:10:42 -0700

    Update 01_python_for_data_I_intro.py

--- New_Code/01_python_for_data_I_intro.py
+++ New_Code/01_python_for_data_I_intro.py
@@ -102,6 +102,61 @@ print (z)
 
 
 
+########################EXERCISE###############################################
+'''
+1. Create your own mad libs game.
+
+2. Place this in a function that you call
+
+3. Later we will learn about inputs if you are curious feel free to google this
+after you complete the exercise to continue updating your code. 
+'''
+## Example Game
+def the_news():
+    a = input('We need your help reporting on the local news! Enter a noun:')
+    b = input('Enter a state:')
+    c = input('Enter a verb:')
+    d = input('Enter a noun:')
+    e = input('Enter a proper name...someones name:')
+    f = input('Enter a verb:')
+    g = input('Enter a noun:')
+    h = input('Enter a verb:')
+    i= input('Enter a noun:')
+    j = input('Enter a part of the body:')
+    k = input('Enter a adjective:')
+    l = input('Enter a relative type:')
+    m = input('Enter a activity:')
+    n = input('Enter a chain resteraunt:')
+    o = input('Enter a adjective past tense:')
+    p = input('Enter a month:')
+    q = input('Enter a verb:')
+    r = input('Enter a noun:')
+    s = input('Enter a verb past tense:') 
+    t = input('Enter a adjective:')
+    u = input('Enter a verb:')
+    v = input('Enter a noun:')
+    w = input('Enter a plural noun:')
+
+    ml= "A {} in {} was arrested this morning after he {} a in front of  {}.\n\
+      {}, had a history of {}, but no one, not even his {} ever imagined \n\
+      he'd {} with a{} stuck in his {}. \n\
+      \n\
+      'I always though he was {}, but I never thought he'd do something \n\
+      like this. Even his {} was surprised.' \n\
+      \n\
+      After a brief {}, cops followed him to a {}, where he reportedly {}\n\
+      in the fry machine. \n\
+      \n\
+      In {}, a woman was charged with a similar crime. But rather than {} \n\
+      a {}, she {} with a {} dog. Either way, we imagine that after  \n\
+      witnessing him {} with a {} there are probably a whole lot of {} \n\
+      that are going to need some therapy.".format(a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w)
+
+    print(ml)
+the_news()
+###############################################################################
+
+
 
 
 
